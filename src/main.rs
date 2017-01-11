8ce254654a3c775315272146ad75d2c558a2f130
Kevin James <KevinJames@thekev.in>
2017-01-11 00:27:05 -0500

    basic file read

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+extern crate env_logger;
+#[macro_use]
+extern crate log;
+
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, World!");
+    env_logger::init().unwrap();
+
+    if env::args().count() != 2 {
+        error!("usage: {} <src_file>", env::args().nth(0).unwrap());
+        std::process::exit(1);
+    }
+
+    let src_file = env::args().nth(1).unwrap();
+    debug!("Using src file {}", src_file);
+
+    let mut file = match File::open(&src_file) {
+        Ok(file) => file,
+        Err(_) => {
+            error!("could not open file: {}", src_file);
+            std::process::exit(1);
+        }
+    };
+
+    let mut src = String::new();
+    match file.read_to_string(&mut src) {
+        Ok(_) => {}
+        Err(_) => {
+            error!("could not read file to string");
+            std::process::exit(1);
+        }
+    };
 }
