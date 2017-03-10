de5434a5be88f6f065eb84c59872e7189242c530
Kevin James <KevinJames@thekev.in>
2017-03-10 07:53:08 -0500

    fix(types): ensure bytes are resolved as primitives

--- src/analysis/types/resolve/primitive.rs
+++ src/analysis/types/resolve/primitive.rs
@@ -10,6 +10,10 @@ lazy_static! {
         let node = ASTNode { token: Token::new(TokenKind::Boolean, None), children: Vec::new() };
         Type::new(ClassOrInterfaceEnvironment::new(node, ClassOrInterface::CLASS))
     };
+    static ref BYTE: Type = {
+        let node = ASTNode { token: Token::new(TokenKind::Byte, None), children: Vec::new() };
+        Type::new(ClassOrInterfaceEnvironment::new(node, ClassOrInterface::CLASS))
+    };
     static ref CHAR: Type = {
         let node = ASTNode { token: Token::new(TokenKind::Char, None), children: Vec::new() };
         Type::new(ClassOrInterfaceEnvironment::new(node, ClassOrInterface::CLASS))
@@ -58,6 +62,7 @@ pub fn go(node: &ASTNode) -> Result<Type, String> {
     match node.token.kind {
         // primitives
         TokenKind::Boolean => Ok(BOOLEAN.clone()),
+        TokenKind::Byte => Ok(BYTE.clone()),
         TokenKind::Char => Ok(CHAR.clone()),
         TokenKind::Int => Ok(INTEGER.clone()),
         TokenKind::Null => Ok(NULL.clone()),
