d012796cfb31259aa6f5a85079ca81190fa664b4
Kevin James <KevinJames@thekev.in>
2017-03-30 19:00:57 -0400

    feat(codegen): mock CastExpression

--- src/generator/body/castexpression.rs
+++ src/generator/body/castexpression.rs
@@ -2,6 +2,8 @@ use std::collections::HashMap;
 
 use scanner::ASTNode;
 
+use super::statement;
+
 pub fn go(node: &ASTNode,
           class_label: &String,
           label: &String,
@@ -11,9 +13,20 @@ pub fn go(node: &ASTNode,
           mut bss: &mut Vec<(String, String)>,
           mut data: &mut Vec<String>)
           -> Result<Option<String>, String> {
-    // TODO<codegen>
-    // Err(format!("NotImplemented CastExpression {:?}", node))
+    match statement::go(&node.children[node.children.len() - 1],
+                        class_label,
+                        label,
+                        fields,
+                        &mut text,
+                        &mut externs,
+                        &mut bss,
+                        &mut data) {
+        Ok(_) => (),
+        Err(e) => return Err(e),
+    }
 
-    // TODO<codegen>: kind is lhs
+    // TODO<codegen>: actual casting
+
+    // TODO<codegen>: kind is lhs (node.children[1] and node.children[2])
     Ok(None)
 }
